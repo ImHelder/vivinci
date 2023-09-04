946bec6ba481fae8c05e7b72f99031d4d1cec269
AdrienLD <adrien.laduca@icloud.com>
2023-09-04 14:56:07 +0200

    Ajout fichier prise date

---
+++ src/Components/PriseRDV/PriseRDV.js
@@ -0,0 +1,20 @@
+import './PriseRDV.css';
+
+function PriseRDV() {
+    return (
+        <div className="PriseRDV">
+            <div className="PriseRDV-date">
+                <p>Choisissez une date</p>
+            </div>
+            <div className="PriseRDV-Infos">
+                <p>Choisissez une date</p>
+            </div>
+            <div className="PriseRDV-Valider">
+                <p>Choisissez une date</p>
+            </div>
+
+        </div>
+    );
+}
+
+export default PriseRDV;
