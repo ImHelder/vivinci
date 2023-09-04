35a26a1b01a3d771c3799511d0b2f00a89d131c3
AdrienLD <adrien.laduca@icloud.com>
2023-09-04 15:14:41 +0200

    modifs mineures

--- src/Components/PriseRDV/PriseRDV.js
+++ src/Components/PriseRDV/PriseRDV.js
@@ -3,13 +3,20 @@ import './PriseRDV.css';
 function PriseRDV() {
     return (
         <div className="PriseRDV">
-            <div className="PriseRDV-date">
+            <button className="PriseRDV-jesuismedecin">Je suis médecin</button>
+            <div className="PriseRDV-infos">
+                <p>Choisissez une date</p>
+                <input type="text" placeholder="Nom" />
+                <input type="text" placeholder="Prénom" />
+                <input type="text" placeholder="Spécialité" />
+                <input type="date" placeholder="Date" />
+                <input type="time" placeholder="Heure" />
+                <input type="text" placeholder="Médecin" />
+            </div>
+            <div className="PriseRDV-image">
                 <p>Choisissez une date</p>
             </div>
-            <div className="PriseRDV-Infos">
-                <p>Choisissez une date</p>
-            </div>
-            <div className="PriseRDV-Valider">
+            <div className="PriseRDV-valider">
                 <p>Choisissez une date</p>
             </div>
 
--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PriseRDV from './Components/PriseRDV/PriseRDV';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App/>} />
+        <Route path="/" element={<PriseRDV/>} />
         <Route path="/test" element={<App/>} />
       </Routes>
     </Router>
