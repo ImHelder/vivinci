3d425166864d48b1242fc8c577bcc55536de15f8
Bastos Gomes Helder <helder@teamr.eu>
2023-09-04 16:03:44 +0200

    css

--- src/Components/PriseRDV/PriseRDV.js
+++ src/Components/PriseRDV/PriseRDV.js
@@ -6,10 +6,9 @@ function PriseRDV() {
         <div className="PriseRDV">
             <button className="PriseRDV-jesuismedecin">Je suis médecin</button>
             <div className="PriseRDV-infos">
-                <p>Choisissez une date</p>
-                <input type="text" placeholder="Nom" />
-                <input type="text" placeholder="Prénom" />
-                <select><option value="Cardiologue">Cardiologue</option>
+                <input className="input" type="text" placeholder="Nom" />
+                <input className="input" type="text" placeholder="Prénom" />
+                <select className='input'><option value="Cardiologue">Cardiologue</option>
                     <option value="Généraliste">Généraliste</option>
                     <option value="Pédiatre">Pédiatre</option>
                     <option value="Urologue">Urologue</option>
@@ -19,13 +18,13 @@ function PriseRDV() {
                     <option value="Aide-soignant">Aide-soignant</option>
                     <option value="Allergologue">Allergologue</option>
                 </select>
-                <input type="date" placeholder="Date" />
-                <select>
+                <input className='input' type="date" placeholder="Date" />
+                <select className='input'>
                     {times.combinations.map(({id, timeRange}) => {
                         return <option value={id}>{timeRange}</option>
                     })}
                 </select>
-                <input type="text" placeholder="Médecin" />
+                <input className='input' type="text" placeholder="Médecin" />
             </div>
             <div className="PriseRDV-image">
                 <p>Choisissez une date</p>
