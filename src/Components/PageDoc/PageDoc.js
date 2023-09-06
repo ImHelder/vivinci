eecb20f0f84304546f75b89fcc3a3253584355b9
AdrienLD <adrien.laduca@icloud.com>
2023-09-06 20:09:40 +0200

    ajout accepter refuser demande

--- src/Components/PageDoc/PageDoc.js
+++ src/Components/PageDoc/PageDoc.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { db } from '../../firebaseConfig'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, doc, getDocs, updateDoc } from 'firebase/firestore'
+import { Block, CheckCircleOutline, Close, Done, HourglassBottom } from '@mui/icons-material';
 
 function PageDoc() {
     const [demandes, setDemandes] = React.useState([]);
+
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
     React.useEffect(() => {
         (async () => {
             const querySnapshot = await getDocs(collection(db, 'demandes'));
@@ -22,13 +26,43 @@ function PageDoc() {
 
     }, []);
 
+    const accepterDemande = async (demandeId, etat) => {
+        const demandeRef = doc(db, 'demandes', demandeId.id);
+        await updateDoc(demandeRef, { etat: etat });
+        setDemandes(demandes.map(demande => demande.id === demandeId ? { ...demande, etat: 'accepte' } : demande));
+    };
+
     return (
         <div>
             <h1>PageDoc</h1>
-            {demandes.map((demande) => (
-                <div key={demande.id}>
-                    <p>{demande.date}</p>
-                    <p>{demande.heure}</p>
+            {demandes.map(demande => (
+                <div className='oneDemande'>
+                    <div className='oneDemandeInfos'>
+                        <p className='dateInfos'>{new Date(demande.date).toLocaleDateString("fr-FR", options)} / {demande.heure} </p>
+                        <p className='medecinInfos'>{demande.idMedecin} / {demande.specialite}</p>
+                    </div>
+                    {
+                        demande.etat === "attente" ? (
+                            <div className="buttons">
+                                <button className='btn' onClick={() => accepterDemande(demande, 'accepte')}><Done /></button>
+                                <button className='btn' onClick={() => accepterDemande(demande, 'refuse')}><Close /></button>
+                            </div>
+
+                        ) : null
+                    }
+
+                    <div className='status'>
+                        {demande.etat === "attente" ? (
+                            <HourglassBottom />
+                        ) : demande.etat === "refuse" ? (
+                            <Block />
+                        ) : demande.etat === "accepte" ? (
+                            <CheckCircleOutline />
+                        ) : (
+                            <p>Pas de statut</p>
+                        )}
+                        <p>{ }</p>
+                    </div>
                 </div>
             ))}
 
