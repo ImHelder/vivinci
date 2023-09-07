00cf38bc577ea5725f40e05e802e90becd1c31bc
Bastos Gomes Helder <helder@teamr.eu>
2023-09-07 09:58:14 +0200

    added image

--- src/Components/SuiviRDV/SuiviRDV.js
+++ src/Components/SuiviRDV/SuiviRDV.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { db } from '../../firebaseConfig'
 import './SuiviRDV.css'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, doc, deleteDoc } from 'firebase/firestore'
 import { useLocation } from 'react-router-dom';
 import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
 import BlockIcon from '@mui/icons-material/Block';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function SuiviRDV() {
 
@@ -13,7 +14,6 @@ function SuiviRDV() {
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
     const location = useLocation();
-    console.log("location", location);
 
     React.useEffect(() => {
         (async () => {
@@ -21,30 +21,44 @@ function SuiviRDV() {
             const _medecins = medecinSnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
 
             const demandeSnapshot = await getDocs(collection(db, 'demandes'));
-            // const _demandes = demandeSnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
-            const _demandes = demandeSnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id, medecin: _medecins.find(medecin => medecin.id === doc.data().idMedecin), startHour: doc.data().heure.split("-")[0].split("h")[0], endHour: doc.data().heure.split("-")[1].split("h")[0] }));
+            const _demandes = demandeSnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id, medecin: _medecins.find(medecin => medecin.id === doc.data().idMedecin), startHour: doc.data().heure.split("-")[0].split("h")[0], endHour: doc.data().heure.split("-")[1].split("h")[0].trim() }));
 
             const userDemandes = _demandes.filter(demande => demande.idClient === location.state.clientId);
-            // const sortedDemandsByDate = userDemandes.sort((a, b) => new Date(b.date) - new Date(a.date));
+            const sortedDemandsByDate = userDemandes.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+            const groups = sortedDemandsByDate.reduce((acc, demande) => {
+                const date = new Date(demande.date).toLocaleDateString("fr-FR");
+                if (!acc[date]) {
+                  acc[date] = [];
+                }
+                acc[date].push(demande);
+                return acc;
+              }, {});
+
+            const sortedDemandByHour = Object.entries(groups).map(([key, value]) => value.sort((a, b) => b.startHour - a.startHour)).flat();
             setDemandes(userDemandes);
         })();
     }, []);
 
-    // const deleteDemande = async (demandeId) => {
-    //     await db.collection('demandes').doc(demandeId).delete();
-    //     setDemandes(demandes.filter(demande => demande.id !== demandeId));
-    // };
-
-    console.log("demandes", demandes)
+    const deleteDemande = async (demandeId) => {
+        const demandeRef = doc(db, 'demandes', demandeId);
+        await deleteDoc(demandeRef);
+        setDemandes(demandes.filter(demande => demande.id !== demandeId));
+    };
 
     return (
         <div className="SuiviRDV">
             <h1>Suivi de vos demandes</h1>
             {demandes.map(demande => (
                 <div className='oneDemande'>
+                    <div className='deleteIcon'>
+                        {demande.etat === "attente" && (
+                            <DeleteIcon onClick={() => deleteDemande(demande.id)} />
+                        )}
+                    </div>
                     <div className='oneDemandeInfos'>
                         <p className='dateInfos'>{new Date(demande.date).toLocaleDateString("fr-FR", options)} / {demande.heure} </p>
-                        <p className='medecinInfos'>{demande.idMedecin} / {demande.specialite}</p>
+                        <p className='medecinInfos'>{demande?.medecin?.nom || demande.idMedecin} / {demande.specialite}</p>
                     </div>
                     <div className='status'>
                         {demande.etat === "attente" ? (
@@ -56,7 +70,6 @@ function SuiviRDV() {
                         ) : (
                             <p>Pas de statut</p>
                         )}
-                        <p>{}</p>
                     </div>
                 </div>
            ))}
