7e4f341058fb2165d8051f52118bd8eabbb880c6
AdrienLD <adrien.laduca@icloud.com>
2023-09-07 17:49:27 +0200

    update tests 9

--- tests/dbdemandes-delete.test.js
+++ tests/dbdemandes-delete.test.js
@@ -4,8 +4,8 @@ import { db } from '../src/firebaseConfig'
 
 describe('fetch clients', () => {
     test('Verify Client Adien LDKA', async () => {
-        const userExist = await getDocs(collection(db, 'demandes')).docs.find(doc => doc.data().client === 'Ceci Est un Test');
-        expect(userExist.id).toBeDefined();
+        const clients = await getDocs(collection(db, 'clients'))
+        const userExist = clients.docs.find(doc => doc.data().prenom === 'Est un Test' && doc.data().nom === 'Ceci');        expect(userExist.id).toBeDefined();
         await deleteDoc(doc(db, "clients", userExist.id));
         const userNotExist = await getDocs(collection(db, 'clients')).docs.find(doc => doc.data().client === 'Ceci Est un Test');
         expect(userNotExist).toBeUndefined();
