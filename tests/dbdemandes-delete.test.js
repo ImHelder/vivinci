7e93ed689085c0f83d9d249058f7f4218b55a716
AdrienLD <adrien.laduca@icloud.com>
2023-09-08 11:19:17 +0200

    update tests 11

--- tests/dbdemandes-delete.test.js
+++ tests/dbdemandes-delete.test.js
@@ -1,15 +1,29 @@
 import { describe, expect, test } from '@jest/globals';
-import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore'
-import { db } from '../src/firebaseConfig'
+import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore';
+import { db } from '../src/firebaseConfig';
 
 describe('fetch clients', () => {
-    test('Verify Client Adien LDKA', async () => {
-        const clients = await getDocs(collection(db, 'clients'))
-        const userExist = clients.docs.find(doc => doc.data().prenom === 'Est un Test' && doc.data().nom === 'Ceci');        
-        expect(userExist.id).toBeDefined();
-        await deleteDoc(doc(db, "clients", userExist.id));
-        const clients2 = await getDocs(collection(db, 'clients'))
-        const userNotExist = clients2.docs.find(doc => doc.data().prenom === 'Est un Test' && doc.data().nom === 'Ceci');
-        expect(userNotExist).toBeUndefined();
+    test('Delete all matching clients', async () => {
+        // Continuer à chercher et supprimer des documents jusqu'à ce qu'il n'y en ait plus
+        let shouldContinue = true;
+        
+        while (shouldContinue) {
+            const clients = await getDocs(collection(db, 'demandes'));
+            const usersToDelete = clients.docs.filter(doc => doc.data().client === 'Ceci Est un Test');
+            
+            if (usersToDelete.length === 0) {
+                shouldContinue = false; // Arrêter la boucle s'il n'y a plus de documents à supprimer
+            } else {
+                for (const user of usersToDelete) {
+                    await deleteDoc(doc(db, "demandes", user.id));
+                }
+            }
+        }
+
+        // Vérifier que tous les documents correspondants ont été supprimés
+        const clientsAfter = await getDocs(collection(db, 'demandes'));
+        const userStillExists = clientsAfter.docs.find(doc => doc.data().client === 'Ceci Est un Test');
+        
+        expect(userStillExists).toBeUndefined();
     });
 });
